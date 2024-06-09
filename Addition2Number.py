e1a38d6cb7a3bab060eab2d420cd839d7c48e210
mamtaJAN <mamtajangid2511@gmail.com>
2024-06-09 13:56:03 +0530

    Addition of 2 number manually&userinput

---
+++ Addition2Number.py
@@ -0,0 +1,13 @@
+num1=1.5
+num2=6.3
+sum=num1+num2
+print("the sum of {0} and {1} is {2}". format(num1,num2,sum))
+print("Sum =", sum)
+
+#user input
+
+n1=input("Enter first number=") 
+n2=input("Enter second number=")
+sum=float(n1)+float(n2)
+print("The sum of {0} and {1} is {2}".format(n1, n2,sum))
+print("Sum=",sum)
