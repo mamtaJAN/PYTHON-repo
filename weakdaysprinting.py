357937373611f4c386b4260825b967d01bc8fd84
mamtaJAN <mamtajangid2511@gmail.com>
2024-06-09 13:50:41 +0530

    weakdays code

---
+++ weakdaysprinting.py
@@ -0,0 +1,17 @@
+x=int(input("enter the value:"))
+if(x==1):
+    print("Sunday")
+elif(x==2):
+    print("Monday")
+elif(x==3):
+    print("Tuesday")
+elif(x==4):
+    print("Wednesday")
+elif(x==5):
+    print("Thrusday")
+elif(x==6):
+    print("Friday")
+elif(x==7):
+    print("Saturday")    
+else:
+    print("not valid")
